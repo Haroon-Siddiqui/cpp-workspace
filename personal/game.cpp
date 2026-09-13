a92589ad3515ec7db4a28389760cd339751b7790
Haroon-Siddiqui <haroonusiddiqui@gmail.com>
2026-09-14 00:19:36 +0500

    added main screen

--- personal/game.cpp
+++ personal/game.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 #ifdef _WIN32
-    #include <windows.h>
+#include <windows.h>
+#endif
 
 using namespace std;
 
@@ -23,10 +24,10 @@ int main ()
                 B A T T L E   S I M U L A T O R
 
 
-Choose your Monster!
+    Choose your Monster!
 
                   1.Wolf    (HP: 100 | DMG :25)
-                  .Dragon  (HP: 160 | DMG :12)
+                  2.Dragon  (HP: 160 | DMG :12)
                   3.Zombie  (HP: 130 | DMG :17)
                   4.Slime   (HP: 120 | DMG :20)
                   5.Golem   (HP: 150 | DMG :15)
