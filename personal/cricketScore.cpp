53998c33b2af90fe1ad6a5eb01d6b30476d3098f
Haroon-Siddiqui <haroonusiddiqui@gmail.com>
2026-09-21 19:34:45 +0500

    Created the General Runs calculator

--- personal/cricketScore.cpp
+++ personal/cricketScore.cpp
@@ -2,5 +2,45 @@
 using namespace std;
 int main ()
 {
+    int runsPerBall,n,totalRuns=0;
+ 
+    // Displaing Mesages and taking input Using for loop ---------------    
+    for ( n=1; n<=6; n++ ){
+
+        cout<<"Enter Runs Scored on Ball no: "<< n <<endl;
     
+       cin>> runsPerBall;
+ 
+       // Calculating Total Runs of the over ---------------
+       totalRuns =totalRuns + runsPerBall;
+ 
+       //Displaying Custom Messages based on Runs Per Ball ---------------
+        if (runsPerBall == 3){
+
+           cout << "Fast Running Between the Wickets" <<endl;
+
+         }
+       else if (runsPerBall == 4){
+          cout << "Nice! a Boundary" <<endl;
+       }
+       else if (runsPerBall == 6 ){
+
+         cout <<"Wow! A sixer" <<endl;
+       }
+    }
+ 
+    // Printing Total Score ---------------
+    cout <<"Total Score is : "<< totalRuns <<endl;    
+ 
+    // Displaying Custom Message based on Total Runs ---------------
+    if (totalRuns >=18){
+        cout << "Batters Smashed the bowler" <<endl;
+    }
+    else if (totalRuns > 6 && totalRuns < 18){
+        cout << "Both Sides Kept the Ground" <<endl;
+    }
+    else if (totalRuns <= 6){
+        cout <<"Tight Bowling From the bowler";
+    }
+          
 }
